3bd62b6cc04c1ec3627536079168448fab48f0fe
Eleazar Meza <meza.eleazar@gmail.com>
2023-04-03 11:53:14 -0400

    Fixed rubocop offenses

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
-gem 'watir', '~>7.2', '>= 7.2.2'
 gem 'csv', '~> 3.2', '>= 3.2.6'
 gem 'pry', '~> 0.14.2'
+gem 'watir', '~>7.2', '>= 7.2.2'
--- buybox.rb
+++ buybox.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'watir'
 require 'csv'
 
@@ -8,13 +10,18 @@ MAX_BUYBOX_COUNT = 10
 PRODUCT_PARSERS = {
   asin: ->(product) { product.data_asin },
   title: ->(product) { product.div(class: 's-title-instructions-style').text },
-  rating_count: ->(product) { product.span(class: 's-underline-text').text.match(/([\d,]+)/).captures.first.delete(',').to_i },
-  buybox_count: ->(product) { },
+  rating_count: lambda { |product|
+    product.span(class: 's-underline-text')
+           .text.match(/([\d,]+)/)
+           .captures.first
+           .delete(',').to_i
+  },
+  buybox_count: ->(product) {},
   url: ->(product) { product.a(class: 'a-link-normal').href }
-}
+}.freeze
 
 url = ARGV[0]
-raise ArgumentError.new 'No URL provided' unless url
+raise ArgumentError, 'No URL provided' unless url
 
 puts 'Starting web browser...'
 browser = Watir::Browser.new :chrome, headless: true
@@ -29,57 +36,62 @@ begin
     result_list = browser.div(class: 's-result-list')
     result_list.wait_until(&:exists?)
 
-    products = result_list.children.select { |r| !r.data_asin.empty? }
+    products = result_list.children.reject { |r| r.data_asin.empty? }
     products.each do |product_html|
-      product = PRODUCT_PARSERS.each_with_object({}) do |(field, parser), product|
-        product[field] = parser.call(product_html)
+      product = PRODUCT_PARSERS.transform_values do |parser|
+        parser.call(product_html)
       end
 
       next unless product[:rating_count] >= MINIMUM_RATING_COUNT
 
-      puts "#{product[:asin]}'s rating count is above #{MINIMUM_RATING_COUNT} (#{product[:rating_count]}), opening product page..."
+      puts "#{product[:asin]}'s rating count is above #{MINIMUM_RATING_COUNT} (#{product[:rating_count]})"
       product_browser.goto product[:url]
 
       buybox_see_all = product_browser.span(id: 'buybox-see-all-buying-choices')
       next unless buybox_see_all.exists?
+
       buybox_see_all.click
 
       puts "#{product[:asin]}'s buybox is disabled...'"
       buybox_count = product_browser.span(id: 'aod-filter-offer-count-string').text.to_i
       next unless buybox_count <= MAX_BUYBOX_COUNT
+
       product[:buybox_count] = buybox_count
 
-      puts "#{product[:asin]}'s seller count is less than #{MAX_BUYBOX_COUNT} (#{product[:buybox_count]}), adding to results..."
+      puts "#{product[:asin]}'s seller count is less than #{MAX_BUYBOX_COUNT} (#{product[:buybox_count]})"
       results << product
     end
 
     next_link = browser.a(class: 's-pagination-next')
     break unless next_link.exists?
 
-    puts "Opening next results page..."
+    puts 'Opening next results page...'
     browser.goto next_link.href
   end
 rescue StandardError => e
   warn 'An error ocurred, saving log to error.json...'
-  File.write('error.json', JSON.dump({
-    error: {
-      message: e.message,
-      backtrace: e.backtrace
-    },
-    search_params: {
-      search_url: url,
-      current_url: browser.url,
-      max_result_pages: MAX_RESULT_PAGES,
-      minimum_rating_count: MINIMUM_RATING_COUNT,
-      max_buybox_count: MAX_BUYBOX_COUNT,
-    }
-  }))
+  File.write('error.json',
+             JSON.dump({
+                         error: {
+                           message: e.message,
+                           backtrace: e.backtrace
+                         },
+                         search_params: {
+                           search_url: url,
+                           current_url: browser.url,
+                           max_result_pages: MAX_RESULT_PAGES,
+                           minimum_rating_count: MINIMUM_RATING_COUNT,
+                           max_buybox_count: MAX_BUYBOX_COUNT
+                         }
+                       }))
 ensure
   product_browser.close
   browser.close
 
-  unless results.empty?
-    puts 'Saving reviews to csv file...'
+  if results.empty?
+    warn 'No product found.'
+  else
+    puts 'Saving products to csv file...'
     CSV.open('results.csv', 'wb', write_headers: true, headers: PRODUCT_PARSERS.keys) do |csv|
       results.each { |review| csv << review }
     end
